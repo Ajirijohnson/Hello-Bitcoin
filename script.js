c97b9a389f0f08d9f52c2af001ebe3a9f3b63d31
Johnson Ajiri <johnsonajiri304@gmail.com>
2022-09-18 01:12:08 +0100

    Updated script branch

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
     
    
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
     
     
     
-        navLinks.forEach((link, index) => {
+//         navLinks.forEach((link, index) => {
        
-            if(link.style.animation) {
-                link.style.animation = '';
-            } else{
-             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
-            }
-         //  console.log(index / 7);
-         });
+//             if(link.style.animation) {
+//                 link.style.animation = '';
+//             } else{
+//              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+//             }
+   
+//          });
     
-         burger.classList.toggle('toggle');
+//          burger.classList.toggle('toggle');
     
+//     });
+    
+    
+    
+    
+    
+//     }
+//     navSlide();
+
+
+     
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+
+ burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards`
+        }
     });
     
-    
-    
-    
-    
-    }
-    navSlide();
-    
+    burger.classList.toggle('toggle');
+ })
+}
+
+
     
     
     
